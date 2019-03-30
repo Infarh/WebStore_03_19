752ab2a41f946e56fd00dd5e2510a70044bfc72e
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-03-30 17:58:14 +0300

    Клиентские библиотеки + основная разметка сайта

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -34,13 +34,6 @@ namespace WebStore
 
             app.UseStaticFiles();
 
-            //var response = Configuration["CustomClientResponse"];
-
-            //app.Run(async (context) =>
-            //{
-            //    await context.Response.WriteAsync(response);
-            //});
-
             app.UseMvc(route =>
             {
                 route.MapRoute(
