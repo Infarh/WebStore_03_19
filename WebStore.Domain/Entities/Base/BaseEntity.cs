90de736c182861f6381696a2a3e9c9cbfda419b4
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-04-10 20:03:46 +0300

    Добавление инфраструктуры EF для сущностей проекта

--- WebStore.Domain/Entities/Base/BaseEntity.cs
+++ WebStore.Domain/Entities/Base/BaseEntity.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using WebStore.Domain.Entities.Base.Interfaces;
 
@@ -8,6 +10,8 @@ namespace WebStore.Domain.Entities.Base
     /// <summary>Сущность</summary>
     public abstract class BaseEntity : IBaseEntity
     {
+        [Key] // Указание на то, что свойство является первичным ключём таблицы
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Требование для БД устанавливать значение данного свойства при добавлении записи в таблицу
         public int Id { get; set; }
     }
 }
--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
@@ -7,8 +8,12 @@ using WebStore.Domain.Entities.Base.Interfaces;
 namespace WebStore.Domain.Entities
 {
     /// <summary>Бренд</summary>
+    [Table("Brands")] // Насильно указываем желаемое имя таблицы. Если этого не сделать, то таблица будет Brand
     public class Brand : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
+
+        // virtual - указание Entity Framework на то, что Products должно быть навигационным свойством!
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
